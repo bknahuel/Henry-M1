142eaa13eb28ea969a8471877d40c99281073b60
bknahuel <boghosian.nahuel@gmail.com>
2022-08-12 22:09:19 -0300

    Homework 03 done

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -1,5 +1,179 @@
 "use strict";
-
+// // function multi(multiplicador){
+// //     //guarda valor privado
+// //     return function( multiplicado){
+// //         return multiplicador * multiplicado;
+// //     }
+// // }
+// ​
+// // var tablaDel2 = multi(2);
+// ​
+// // console.log(tablaDel2(1));
+// ​
+// ​
+// // // console.log(tablaDel2(2));
+// ​
+// // var tablaDel7 = multi(7); //guardo la declaracion de la funcion interna
+// // var tablaDel9 = multi(9);
+// // var tabal9 = [];
+// // for (let i = 1; i < 11; i++) {
+// //     tabal9.push(tablaDel9(i));
+// // }
+// // console.log(tabal9);
+// ​
+// ​
+// // var creaFuncion = function(){
+// // 	var arreglo = [];
+ 
+// // 	for ( var i=0; i < 3; i++){
+// // 		arreglo.push(
+// // 			function(){
+// // 				console.log(i);
+// // 			}
+// // 		)
+// // 	}
+// // 	return arreglo;
+// // }
+ 
+// // var arr = creaFuncion();
+// ​
+// // arr[0](); //0
+// // arr[1](); //0
+// // arr[2](); //0
+// ​
+// // var creaFuncion = function(){
+// // 	var arreglo = [];
+// // 	for ( var i=0; i < 3; i++){
+// //       	// IIFE
+// // 		arreglo.push(
+// // 			(function(j){
+// // 				return function() {console.log(j);}
+// // 			}(i))
+// // 		)
+// // 	}
+// // 	return arreglo;
+// // }
+// ​
+// // var arr = creaFuncion();
+// ​
+// // arr[0]() // 0
+// // arr[1]() // 1
+// // arr[2]() // 2
+// ​
+// // function hacerSaludo( lenguaje ){
+// // 	if ( lenguaje === 'en'){
+// // 		return function(nombre){
+// // 			console.log('Hi!'  + ' ' + nombre );
+// // 		}
+// // 	}
+ 
+// // 	if ( lenguaje === 'es'){
+// // 		return function(nombre){
+// // 			console.log('Hola!' + ' ' + nombre );
+// // 		}
+// // 	}
+// // }
+ 
+// // var saludoIngles = hacerSaludo('en');
+// // var saludoEspaniol = hacerSaludo('es');
+// ​
+// // saludoIngles('Chris');
+// // saludoEspaniol('Chris');
+// ​
+// //THIS!!!!!
+// ​
+// // var obj = {
+// //     nombre: 'Objeto',
+// //     log   : function(){
+// //       this.nombre = 'Cambiado'; // this se refiere a este objeto, a `obj`
+// //       console.log(this)  // obj
+  
+// //       var cambia = function( str ){
+// //         this.nombre = str;  // Uno esperaria que this sea `obj`
+// //       }
+  
+// //       cambia('Hoola!!');
+// //       console.log(this);
+// //     }
+// // }
+// ​
+// // obj.log();
+// ​
+// // var persona = {
+// // 	nombre: 'Guille',
+// // 	apellido: 'Aszyn',
+// // }
+// ​
+// // var animal = {
+// //     nombre: 'Gato',
+// //     apellido: 'Lucifer'
+// // }
+// ​
+// // var logNombre = function(str){
+// //     this.apellido = str;
+// // 	console.log(this.apellido);
+// // }
+// ​
+// // var logNombrePersona = logNombre.bind(persona); //this = persona
+// // // el primer parametro de bind es el this!
+// // logNombrePersona('Gomez');
+// ​
+// // var logNombreAnimal = logNombre.bind(animal); //this = persona
+// ​
+// // logNombreAnimal('El jeringas');
+// ​
+// ​
+// // BIND DEVUELVE UNA FUNCION!
+// ​
+// // function multiplica(a, b){
+// // 	return a * b;
+// // }
+// ​
+// // var multiplicaPorDos = multiplica.bind(this, 2); //(ojeto, a)
+// ​
+// // console.log(multiplicaPorDos(3)) //b
+// ​
+// // console.log(multiplica(5, 4));
+// // el Bind le `bindeó` el 2 al argumento a.
+// // y devolvió una función nueva con ese parámetro bindeado.
+// ​
+// var persona = {
+// 	nombre: 'Guille',
+// 	apellido: 'Aszyn',
+// }
+// ​
+// var logNombre = function(){
+// 	console.log(this.nombre);
+// }
+// ​
+// // el primer parametro de call es el this!
+// logNombre.call(persona);
+// ​
+// // Call hace lo mismo que Bind, solo que invoca la función,
+// // no devuelve una nueva.
+// // tambien bindea argumentos!
+// // 
+// var logNombre = function(arg1, arg2){
+// 	return (arg1 +' '+ this.nombre +' '+ arg2);
+// }
+// ​
+// var result = logNombre.call(persona, 'Hola', ', Cómo estas?');
+// ​
+// console.log(result);
+// ​
+// // Apply es igual a call, solo que el segundo argumento es un
+// // arreglo.
+// ​
+// var logNombre = function(arg1, arg2){
+// 	console.log(arg1 +' '+ this.nombre +' '+ arg2);
+// }
+// ​
+// var miArr = ['Hola', ', Cómo estas?'];
+// ​
+// logNombre.apply(persona, miArr);
+// //Hola Guille , Cómo estas?
+// ​
+// ////Hola Guille, Cómo estas?
 // Closures
 
 function counter() {
@@ -19,29 +193,33 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  let contador = 0; 
+  return function nuevoContador(){
+    contador ++;
+    return contador;
+  }
 }
 
 function cacheFunction(cb) {
-  /*
-  Ejercicio 2
-
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
-
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
-
-
-  Ejemplo:
-  function square(n){
-    return n * n
+  //Callback = Es una funcion la cual es utilizada como parametro en otra funcion. 
+  // let showCache = 0;
+  // return {
+  //   cb: function(){
+  //     showCache++;
+  //   }
+  // }
+  var cache = {}
+  return function (arg) {
+    if(cache.hasOwnProperty(arg)){
+      return cache[arg];
+    } else {
+      cache[arg] = cb(arg);
+      return cache[arg];
+    }
   }
-
-  const squareCache = cacheFunction(square)
-
-  squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
-  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
-
-  */
 }
+
+// return condicion ? sentenciaTrue : sentenciaFalse;
 
 // Bind
 
@@ -67,8 +245,14 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
+
+getNombreInstructor();
+getNombreAlumno();
+
+
+
 
 /*
   Ejercicio 4
@@ -80,9 +264,10 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(this, '*', '*');
+let textoGuiones = crearCadena.bind(this, '-', '-');
+let textoUnderscore = crearCadena.bind(this, '_', '_');
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
